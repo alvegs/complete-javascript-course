fe0e407b0058e86dd8fb7613f36aef7a09349461
aleksander vegsund <a.vegsund@gmail.com>
2023-12-21 20:30:33 +0100

    added script to fundamentals part1 - finished part

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,66 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('jonas');
+console.log(23);
+
+let firstName = 'Matilda'; // variable holding a name string
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// variable name conventions
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'coder';
+let myCurrentJob = 'teacher';
+
+console.log(myFirstJob);
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'jonas');
+
+let year;
+console.log(typeof year); // typen til year... er undefined
+console.log(year); // verdien til year... er undefined
+
+year = 1991;
+console.log(typeof year);
+
+
+const ageJonas = 2037 - 1991;
+const ageSarah = 2037 - 2018;
+console.log(ageJonas, ageSarah);
+
+// assignment operators
+let x = 10 + 4; // value 14
+x = 12; // change value to 12
+x += 12; // 24
+x /= 2; // 12 again (x = x / 2; );
+
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean(''));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision);
+*/
