c5ed8ebb043e00b9c55f245b20c4957bab9d27b5
aleksander vegsund <a.vegsund@gmail.com>
2024-01-24 20:16:51 +0100

    done behind the scenes section

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function calcAge(birth) {
+  const age = 2024 - birth;
+  console.log(age);
+}
+calcAge(1991);
+
+const calcAgeArrow = birth => {
+  const age = 2024 - birth;
+  console.log(age);
+  console.log(this);
+};
+calcAgeArrow(1991);
