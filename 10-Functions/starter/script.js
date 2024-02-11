0011da075a6218daae35f93b3b7aee091aa35330
aleksander vegsund <a.vegsund@gmail.com>
2024-02-11 08:03:39 +0100

    started part 10

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -16,7 +16,7 @@ createBooking('LH123');
 createBooking('LH123', 2, 800); */
 
 // ARGUMENT BY VALUE VS. BY REFERENCE (there is only pass by value in JavaScript, no pass by reference except)
-const flight = 'LH234';
+/* const flight = 'LH234';
 const jonas = {
   name: 'Jonas Schmedtmann',
   passport: 23444232457,
@@ -42,4 +42,181 @@ const newPassport = function (person) {
 };
 
 newPassport(jonas);
-//checkIn(flight, jonas);
+//checkIn(flight, jonas); */
+
+// FUNCTIONS ACCEPTING CALLBACK FUNCTIONS
+/* const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// higher order function (one that takes function as parameter)
+const transformer = function (str, fn) {
+  console.log(`original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best', upperFirstWord);
+transformer('JavaScript is the best', oneWord);
+
+// JS uses callback functions all the time
+const high5 = function () {
+  console.log('high 5');
+};
+
+document.body.addEventListener('click', high5); */
+
+// FUNCTIONS THAT RETURN OTHER FUNCTIONS
+/* const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('hey');
+greeterHey('jonas');
+greeterHey('steven');
+
+greet('hello')('jonas');
+
+// rewrite greet using arrow functions
+const greetTwo = greeting => {
+  return name => {
+    console.log(`greetingTwo: ${greeting} ${name}`);
+  };
+};
+
+greetTwo('hellooooo')('uncle Leo!'); */
+
+// THE CALL AND APPLY METHODS
+/* const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(636, 'John Smith');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does not work as intended
+// book(23, 'Sarah Williams');
+
+book.call(eurowings, 23, 'sarah williams');
+book.call(lufthansa, 239, 'mary cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'aleks veg');
+
+// apply method - same ass call but takes array after spesifying what object to call(swiss in this case)
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData); // spread operator used on array
+
+// Bind method
+// book.call(eurowings, 23, 'sarah williams');
+const bookEW = book.bind(eurowings); // this binds the book function to the eurowings object
+bookEW(23, 'Steven Williams'); // does not need to specify object via params after that
+
+// bind the function for the rest of the objects
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+const bookEW23 = book.bind(eurowings, 23); // here we bind the function for eurowings AND the flight number
+bookEW23('aleksander');
+
+// with event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => {
+  return value + value * rate;
+};
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+
+// rewrite this - my challenge solution
+const addTaxTwo = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+// working correctly
+const addVAT2 = addTaxTwo(0.23)(100);
+console.log(addVAT2); */
+
+// CODING CHALLENGE #1
+const poll = {
+  question: 'what is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates array with [0, 0, 0, 0]
+  answers: new Array(4).fill(0),
+};
+
+// display the reuslts in answers from poll object
+const displayResults = function (array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+};
+
+// create method for registering new options
+poll.registerNewAnswer = function (answer) {
+  prompt(`${poll.question} 
+  ${poll.options[0]}
+  ${poll.options[1]}
+  ${poll.options[2]}
+  ${poll.options[3]}
+  (write option number)`);
+
+  if (typeof answer === Number) {
+    if (answer >= 0 && answer <= poll.options.length) {
+      poll.answers[answer] += 1;
+    }
+  }
+  displayResults(poll.answers);
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer);
